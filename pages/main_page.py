ab8c7efed1b38826ade161702b8ead1f8c1b8023
Aslan <baristan1@rambler.ru>
2020-09-22 23:27:36 +0300

    add some file for pattern POM

---
+++ pages/main_page.py
@@ -0,0 +1,9 @@
+from .base_page import BasePage
+from pages.login_page import LoginPage
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+
+
+class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
