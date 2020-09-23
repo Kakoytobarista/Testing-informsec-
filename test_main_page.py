8e829010444bfc45fa82388397db60980d30041f
Aslan <baristan1@rambler.ru>
2020-09-23 18:25:12 +0300

    add test with checking manager window

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,19 @@
 import pytest
 from pages.base_page import BasePage
+from pages.other_page import MainPageAsserts
 from pages.main_page import MainPage
 
 main_link = "https://itsecurity.ru/"
 
 
 @pytest.mark.login_guest
-class TestLoginFromMainPage(object):
-    def test_guest_can_see_main_page(self, browser):
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_main_page(self, browser):
         page = MainPage(browser, main_link)
         page.open()
 
+    def test_guest_can_ask_the_manager_a_question(self, browser):
+        page = MainPageAsserts(browser, main_link)
+        page.open()
+        page.go_to_feedback_page()
+        page.should_see_ask_questions_manager()
