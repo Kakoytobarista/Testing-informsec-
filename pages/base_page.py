3ddfbd9ae7d1f6d263a5761c7a3fc049f70ae387
Aslan <baristan1@rambler.ru>
2020-09-23 16:14:04 +0300

    add function with open site

---
+++ pages/base_page.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage(object):
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False, "element is not present"
+        return True
