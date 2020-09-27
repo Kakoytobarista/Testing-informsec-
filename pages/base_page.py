c6bfd283de59920b3c95bdfec477d9c4b5ceeb49
Aslan <baristan1@rambler.ru>
2020-09-27 18:27:04 +0300

    add 3 new methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,9 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.locators import MainPageLocators
+from pages.locators import LoginPageLocators
+from pages.private_value import password
+from pages.private_value import email
 
 
 class BasePage(object):
@@ -26,3 +29,15 @@ class BasePage(object):
     def go_to_feedback_page(self):
         link = self.browser.find_element(*MainPageLocators.ASK_MANAGER)
         link.click()
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+
+    def register_on_login_page(self):
+        input1 = self.browser.find_element(*LoginPageLocators.LOGIN_USER_SELECTOR)
+        input1.send_keys(email)
+        input2 = self.browser.find_element(*LoginPageLocators.PASSWORD_USER_SELECTOR)
+        input2.send_keys(password)
+        self.browser.find_element(*LoginPageLocators.BUTTON_LOGIN_SELECTOR).click()
+
