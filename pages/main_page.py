8b50a044206b1169e1d7b19ddb55a969be94f9d7
Aslan <baristan1@rambler.ru>
2020-09-27 18:32:05 +0300

    add new assert for main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,13 @@ from selenium.webdriver.common.by import By
 from pages.locators import MainPageLocators
 
 
-class MainPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
+# class MainPage(BasePage):
+#     def __init__(self, *args, **kwargs):
+#         super(MainPage, self).__init__(*args, **kwargs)
 
+
+class MainPage(BasePage):
+    def should_be_main_url(self):
+        str_url = self.url
+        print(str_url)
+        assert "https://itsecurity.ru/" in str_url, "main page do not find in URL"
