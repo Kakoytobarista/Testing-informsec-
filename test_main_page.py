f5e70fa3c9313c8cb80ed82c6666d883b1226669
Aslan <baristan1@rambler.ru>
2020-09-27 21:54:36 +0300

    add test with checking tab access

--- test_main_page.py
+++ test_main_page.py
@@ -3,10 +3,10 @@ from pages.base_page import BasePage
 from pages.other_page import MainPageAsserts
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+
 login_link = "https://itsecurity.ru/personal/"
 main_link = "https://itsecurity.ru/"
 from py.xml import html
-
 
 
 @pytest.mark.login_guest
@@ -28,11 +28,20 @@ class TestFromMainPage(object):
     #     login = LoginPage(browser, login_link)
     #     login.go_to_login_page()
     #     login.should_be_login_page()
+    #
+    # def test_guest_can_log_in(self, browser):
+    #     page = LoginPage(browser, login_link)
+    #     page.open()
+    #     page.go_to_login_page()
+    #     page.register_on_login_page()
+    #     page.should_see_successful_login_in()
 
-    def test_guest_can_log_in(self, browser):
-        page = LoginPage(browser, login_link)
+    @pytest.mark.parametrize('link', ["https://itsecurity.ru/",
+                                      "https://itsecurity.ru/courses/",
+                                      "https://itsecurity.ru/services/",
+                                      "https://itsecurity.ru/informatsionnaya-bezopasnost-povyshenie-osvedomlennosti/",
+                                      "https://itsecurity.ru/contacts/",
+                                      "https://itsecurity.ru/study-center/"])
+    def test_guest_can_access_all_tabs(self, browser, link):
+        page = BasePage(browser, link)
         page.open()
-        page.go_to_login_page()
-        page.register_on_login_page()
-        page.should_see_successful_login_in()
-
