716c20e52ac0c51549fad4c5825d3ec273c3be1e
Aslan <baristan1@rambler.ru>
2020-09-27 18:25:22 +0300

    add 3 new tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,33 +1,38 @@
 import pytest
-import allure
-from allure_commons.types import AttachmentType
 from pages.base_page import BasePage
 from pages.other_page import MainPageAsserts
 from pages.main_page import MainPage
-
+from pages.login_page import LoginPage
+login_link = "https://itsecurity.ru/personal/"
 main_link = "https://itsecurity.ru/"
+from py.xml import html
+
 
 
 @pytest.mark.login_guest
-class TestLoginFromMainPage(object):
-    @allure.feature('Open pages')
-    @allure.story('Тестируем вход на страницу сайтй')
-    @allure.severity('critical')
-    def test_guest_can_go_to_main_page(self, browser):
-        page = MainPage(browser, main_link)
-        page.open()
-        with allure.step('Делаем скриншот'):
-            allure.attach(browser.get_screenshot_as_png(),
-                          name="Screenshot", attachment_type=AttachmentType.PNG)
+class TestFromMainPage(object):
+    # def test_guest_can_go_to_main_page(self, browser):
+    #     page = MainPage(browser, main_link)
+    #     page.open()
+    #     page.should_be_main_url()
+    #
+    # def test_guest_can_ask_the_manager_a_question(self, browser):
+    #     page = MainPageAsserts(browser, main_link)
+    #     page.open()
+    #     page.go_to_feedback_page()
+    #     page.should_see_ask_questions_manager()
+    #
+    # def test_guest_can_go_to_login_page(self, browser):
+    #     page = MainPage(browser, main_link)
+    #     page.open()
+    #     login = LoginPage(browser, login_link)
+    #     login.go_to_login_page()
+    #     login.should_be_login_page()
 
-    @allure.feature('Open pages')
-    @allure.story("Тестируем открытие окна диалога с менеджером")
-    @allure.severity('blocker')
-    def test_guest_can_ask_the_manager_a_question(self, browser):
-        page = MainPageAsserts(browser, main_link)
+    def test_guest_can_log_in(self, browser):
+        page = LoginPage(browser, login_link)
         page.open()
-        page.go_to_feedback_page()
-        with allure.step('Делаем скриншот'):
-            allure.attach(browser.get_screenshot_as_png(),
-                          name="Screenshot", attachment_type=AttachmentType.PNG)
-        page.should_see_ask_questions_manager()
+        page.go_to_login_page()
+        page.register_on_login_page()
+        page.should_see_successful_login_in()
+
