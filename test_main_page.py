276eb7f5a7bbbbaf250ae2c3a348f34e7250a071
Aslan <baristan1@rambler.ru>
2020-09-28 17:36:11 +0300

    add new test for emprove failed work a tab

--- test_main_page.py
+++ test_main_page.py
@@ -3,6 +3,8 @@ from pages.base_page import BasePage
 from pages.other_page import MainPageAsserts
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+import time
+
 
 login_link = "https://itsecurity.ru/personal/"
 main_link = "https://itsecurity.ru/"
@@ -35,13 +37,20 @@ class TestFromMainPage(object):
     #     page.go_to_login_page()
     #     page.register_on_login_page()
     #     page.should_see_successful_login_in()
-
-    @pytest.mark.parametrize('link', ["https://itsecurity.ru/",
-                                      "https://itsecurity.ru/courses/",
-                                      "https://itsecurity.ru/services/",
-                                      "https://itsecurity.ru/informatsionnaya-bezopasnost-povyshenie-osvedomlennosti/",
-                                      "https://itsecurity.ru/contacts/",
-                                      "https://itsecurity.ru/study-center/"])
-    def test_guest_can_access_all_tabs(self, browser, link):
-        page = BasePage(browser, link)
+    #
+    # @pytest.mark.parametrize('link', ["https://itsecurity.ru/",
+    #                                   "https://itsecurity.ru/courses/",
+    #                                   "https://itsecurity.ru/services/",
+    #                                   "https://itsecurity.ru/informatsionnaya-bezopasnost-povyshenie-osvedomlennosti/",
+    #                                   "https://itsecurity.ru/contacts/",
+    #                                   "https://itsecurity.ru/study-center/"])
+    # def test_guest_can_access_all_tabs(self, browser, link):
+    #     page = BasePage(browser, link)
+    #     page.open()
+    def test_guest_can_write_to_edu(self, browser):
+        page = MainPage(browser, main_link)
         page.open()
+        page.click_to_edu()
+        page.fill_in_the_email(browser)
+        page.should_element_is_not_present()
+
