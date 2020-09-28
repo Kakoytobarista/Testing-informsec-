87b56cea07435e7a49b836476a41bd8e32efb909
Aslan <baristan1@rambler.ru>
2020-09-28 17:38:19 +0300

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 
 class MainPageLocators(object):
@@ -6,7 +7,20 @@ class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "body > div.main > div.header > div.b-input > ul > li.input > span > a")
     CATALOG_COURSE = (By.CSS_SELECTOR, "body > div.main > div.panel-top > div > \
     div.menu-top > ul > li:nth-child(1) > a")
+    BUTTON_AT_EDU = (By.CSS_SELECTOR, "#jvlabelWrap > jdiv.hoverl_e8c")
+    FILL_THE_EMAIL = (By.CSS_SELECTOR, "#jcont > jdiv.contentWrapper_22c > jdiv > jdiv.input_878.show_fa0 > jdiv"
+                                       " > jdiv.tdTextarea_bac > textarea")
+    FEEDBACK_FROM_EDU = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/"
+    "jdiv[3]/jdiv/text()")
 
+    BUTTON_FOR_EDU = (By.CSS_SELECTOR, "#jcont > jdiv.contentWrapper_22c > jdiv > jdiv.input_878.show_fa0 > jdiv > "
+                                       "jdiv:nth-child(2) > jdiv")
+
+    FILL_FOR_EDU = (By.CSS_SELECTOR, "#scrollbar-container > jdiv:nth-child(1) > jdiv > jdiv.box_e16 \
+                                     > jdiv > jdiv > jdiv.overlay_6f8")
+
+    FIL_FOR_EDU2 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[1]/input")
+    FIL_FOR_EDU3 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[1]/input")
 
 
 class OtherPageLocators(object):
