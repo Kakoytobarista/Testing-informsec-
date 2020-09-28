16bcfc3cf8dcf7b988eadd53c0d9ee3c768fb475
Aslan <baristan1@rambler.ru>
2020-09-29 01:23:24 +0300

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -7,20 +7,30 @@ class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "body > div.main > div.header > div.b-input > ul > li.input > span > a")
     CATALOG_COURSE = (By.CSS_SELECTOR, "body > div.main > div.panel-top > div > \
     div.menu-top > ul > li:nth-child(1) > a")
-    BUTTON_AT_EDU = (By.CSS_SELECTOR, "#jvlabelWrap > jdiv.hoverl_e8c")
+    BUTTON_AT_EDU = (By.CSS_SELECTOR, "#jvlabelWrap > jdiv.text_ff3._offline_9d2.contentTransitionWrap_b43")
     FILL_THE_EMAIL = (By.CSS_SELECTOR, "#jcont > jdiv.contentWrapper_22c > jdiv > jdiv.input_878.show_fa0 > jdiv"
                                        " > jdiv.tdTextarea_bac > textarea")
-    FEEDBACK_FROM_EDU = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/"
+    FEEDBACK_FROM_EDU = (By.CSS_SELECTOR, "#jvlabelWrap > jdiv.hoverl_e8c"
     "jdiv[3]/jdiv/text()")
 
     BUTTON_FOR_EDU = (By.CSS_SELECTOR, "#jcont > jdiv.contentWrapper_22c > jdiv > jdiv.input_878.show_fa0 > jdiv > "
                                        "jdiv:nth-child(2) > jdiv")
 
-    FILL_FOR_EDU = (By.CSS_SELECTOR, "#scrollbar-container > jdiv:nth-child(1) > jdiv > jdiv.box_e16 \
-                                     > jdiv > jdiv > jdiv.overlay_6f8")
+    FILL_FOR_EDU = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv[2]/jdiv/jdiv[1]/input")
 
-    FIL_FOR_EDU2 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[1]/input")
-    FIL_FOR_EDU3 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[1]/input")
+    FILL_FOR_EDU2 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[1]/input")
+    FILL_FOR_EDU3 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[2]")
+    INFORMATION_AT_TOP = (By.XPATH, "/html/body/div[3]/div[1]/div[2]")
+    INFORMATION_AT_BOTTOM1 = (By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(1)")
+    INFORMATION_AT_BOTTOM2 = (By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(2)")
+    INFORMATION_AT_BOTTOM3 = (By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(3)")
+    TABLE_OF_NEWS = (By.CSS_SELECTOR, "body > div.main > div.content > div.lent-news > div.slide-news >\
+                                      div.jcarousel-skin-tango > div")
+    TABLE_WITH_SCHEDULE_COURSES = (By.CSS_SELECTOR, "body > div.main > div.content > div.main-content >\
+                                                      div.center > div.courses")
+    MESSAGE_AFTER_ALL_PROCEDURE = (By.CSS_SELECTOR, "#scrollbar-container > jdiv:nth-child(1)\
+                                                     > jdiv > jdiv.box_e16.__noanim_1b1 > jdiv > jdiv > jdiv > jdiv\
+                                                     > jdiv.buttonWrapper_2b2.__fadeOut_5f5 > jdiv")
 
 
 class OtherPageLocators(object):
