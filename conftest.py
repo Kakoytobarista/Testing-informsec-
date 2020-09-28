4bab6bfed1ec96691a1daea0777f25464f494e3a
Aslan <baristan1@rambler.ru>
2020-09-29 01:20:18 +0300

    delete fixture

--- conftest.py
+++ conftest.py
@@ -25,21 +25,6 @@ def browser(request):
     browser.quit()
 
 
-# @pytest.hookimpl(hookwrapper=True)
-# def pytest_runtest_makereport(item, call):
-#     pytest_html = item.config.pluginmanager.getplugin('html')
-#     outcome = yield
-#     report = outcome.get_result()
-#     extra = getattr(report, 'extra', [])
-#     if report.when == 'call':
-#         # always add url to report
-#         extra.append(pytest_html.extras.url('http://www.example.com/'))
-#         xfail = hasattr(report, 'wasxfail')
-#         if (report.skipped and xfail) or (report.failed and not xfail):
-#             # only add additional html on failure
-#             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
-#         report.extra = extra
-
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item):
     timestamp = datetime.now().strftime('%H-%M-%S')
