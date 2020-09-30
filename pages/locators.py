95735a7c54f3164ac08dc7cc8c5f44110bcbf6b6
Aslan <baristan1@rambler.ru>
2020-09-30 14:53:02 +0300

    add new selectors

--- pages/locators.py
+++ pages/locators.py
@@ -11,19 +11,23 @@ class MainPageLocators(object):
     FILL_THE_EMAIL = (By.CSS_SELECTOR, "#jcont > jdiv.contentWrapper_22c > jdiv > jdiv.input_878.show_fa0 > jdiv"
                                        " > jdiv.tdTextarea_bac > textarea")
     FEEDBACK_FROM_EDU = (By.CSS_SELECTOR, "#jvlabelWrap > jdiv.hoverl_e8c"
-    "jdiv[3]/jdiv/text()")
+                                          "jdiv[3]/jdiv/text()")
 
     BUTTON_FOR_EDU = (By.CSS_SELECTOR, "#jcont > jdiv.contentWrapper_22c > jdiv > jdiv.input_878.show_fa0 > jdiv > "
                                        "jdiv:nth-child(2) > jdiv")
 
-    FILL_FOR_EDU = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv[2]/jdiv/jdiv[1]/input")
+    FILL_FOR_EDU = (
+    By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv[2]/jdiv/jdiv[1]/input")
 
     FILL_FOR_EDU2 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[1]/input")
     FILL_FOR_EDU3 = (By.XPATH, "//*[@id='scrollbar-container']/jdiv[1]/jdiv/jdiv[4]/jdiv/jdiv/jdiv/jdiv/jdiv[2]")
     INFORMATION_AT_TOP = (By.XPATH, "/html/body/div[3]/div[1]/div[2]")
-    INFORMATION_AT_BOTTOM1 = (By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(1)")
-    INFORMATION_AT_BOTTOM2 = (By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(2)")
-    INFORMATION_AT_BOTTOM3 = (By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(3)")
+    INFORMATION_AT_BOTTOM1 = (
+    By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(1)")
+    INFORMATION_AT_BOTTOM2 = (
+    By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(2)")
+    INFORMATION_AT_BOTTOM3 = (
+    By.CSS_SELECTOR, "body > div.footer > div.contacts-f > div.b-adress > ul > li:nth-child(3)")
     TABLE_OF_NEWS = (By.CSS_SELECTOR, "body > div.main > div.content > div.lent-news > div.slide-news >\
                                       div.jcarousel-skin-tango > div")
     TABLE_WITH_SCHEDULE_COURSES = (By.CSS_SELECTOR, "body > div.main > div.content > div.main-content >\
@@ -47,3 +51,20 @@ class LoginPageLocators(object):
     BUTTON_LOGIN_SELECTOR = (By.CSS_SELECTOR, "body > div.main > div.content > div.inner > div > div:nth-child(1) > "
                                               "div > form > div.button.auth-submit")
     SUCCESSFUL_LOGIN = (By.CSS_SELECTOR, "body > div.main > div.content > div.color > h2")
+    REGISTER_INFORMATION = (By.CSS_SELECTOR, "#user_div_reg > table > tbody")
+
+
+class ProductPageLocators(object):
+    BASKET_LINK = (By.CSS_SELECTOR, "body > div.main-bask.bsk > div > div > div.bs > ul > li:nth-child(1) > a")
+    BASKET_STATUS = (By.XPATH, "/html/body/script[19]/text()")
+    COURSE_PAGE = (By.CSS_SELECTOR, "body > div.main > div.panel-top > div > div.menu-top > ul > li:nth-child(1) > a")
+    COURSE_PRODUCT = (By.CSS_SELECTOR, "body > div.main > div.content > div.inner > div > div.table-time\
+                                        > table > tbody > tr:nth-child(2) > td.name-c > a")
+    PRODUCT_OF_COURSE_TAKE_TO_BASKET = (By.CSS_SELECTOR, "#table-wrap > tbody > tr > td > div\
+                                                          > div > div > div > div > a")
+    CATALOG_COURSES_STUFF = (By.XPATH, "/html/body/div[3]/div[3]/div[3]/div/h3/text()")
+    CALENDAR_OF_COURSE = (By.NAME, "date_from")
+    BUTTON_OF_CALENDAR = (By.CSS_SELECTOR, "#table-wrap > tbody > tr > td > div > div > div > div > div > a")
+    BUTTON_TAKE_TO_BASKET = (By.CSS_SELECTOR, "#cboxLoadedContent > div > div:nth-child(1) > a")
+    PRODUCT_IN_BASKET = (By.XPATH, "/html/body/div[3]/div[3]/div[2]/form/div[4]/div[1]/table/tbody/tr/td[1]/div/a")
+    BASKET_INFORMATION_AT_TOP_SIDE = (By.CSS_SELECTOR, "body > div.main > div.content > div.inner > form > div.info-c")
