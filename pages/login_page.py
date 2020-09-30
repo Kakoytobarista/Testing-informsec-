0e3ff611a18dc7a18c70b741d9bb8e21b35f0809
Aslan <baristan1@rambler.ru>
2020-09-30 14:53:26 +0300

    add some fix stuff

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,6 +7,7 @@ class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
+        self.should_see_successful_login_in()
 
     def should_be_login_url(self):
         str_url = self.url
@@ -18,4 +19,9 @@ class LoginPage(BasePage):
 
     def should_see_successful_login_in(self):
         assert self.is_element_present(*LoginPageLocators.SUCCESSFUL_LOGIN), "should see 'Настройка личного кабинета'"
+        print('\nchecking status registration')
 
+    def should_be_register_information(self, driver):
+        driver.execute_script("window.scrollTo(0, 400)")
+        print('\nfind register information')
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INFORMATION), "should be register information"
